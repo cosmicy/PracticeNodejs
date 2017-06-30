0e801131546ea5d4f4f705f84c9edc323bc27caf
cytdqs <cosmicy@outlook.com>
2017-06-30 16:53:00 +0800

    *文字换为图片

--- electron/cydemo/app/js/cy.js
+++ electron/cydemo/app/js/cy.js
@@ -4,7 +4,11 @@ function cy() {
 	body = document.getElementById('body'),
 	list = document.createElement('div'),
 	step1 = document.createElement('div'),
-	step2 = document.createElement('div');
+	step2 = document.createElement('div'),
+
+	img1 = document.createElement('img'),
+	img2 = document.createElement('img');
+
 	
 	list.id = 'impress';
 
@@ -20,7 +24,11 @@ function cy() {
 	step2.setAttribute('class', 'step');
 	step2.setAttribute('data-x', '1000');
 	step2.setAttribute('data-y', '800');
-	step2.innerText = 'Haskell2';
+	
+	img2.setAttribute('src', 'img/temp_ppt/temp_ppt_1.jpg');
+	step2.appendChild(img2)
+
+	//step2.innerText = 'Haskell2';
 	list.appendChild(step2);
 
 	impress().init();
