afe67f03d7bb63b0d8a387c5806a9aa14fe112ad
cytdqs <cosmicy@outlook.com>
2017-07-20 11:56:56 +0800

    *增加自己的vue模块

---
+++ Vue.js/cyproj/src/components/cy.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="cy">
+      <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cy',
+  data () {
+    return {
+      msg: 'wendy'
+    }
+  }
+}
+</script>
